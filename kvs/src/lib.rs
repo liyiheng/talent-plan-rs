513f0e2201b89c737927454f40dce370f92b378f
liyiheng <liyihenggnehiyil@gmail.com>
2020-01-16 21:30:06 +0800

    Project 2: Make the test cases build

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,41 +1,41 @@
 //! kvs is a lib for in-memory kv storage
 #![deny(missing_docs)]
-use std::collections::HashMap;
+pub use std::io::Result;
+use std::path::PathBuf;
 
-/// KvStore keeps all K-V pairs in a HashMap
-pub struct KvStore {
-    data: HashMap<String, String>,
-}
+/// KvStore provides all functions of this lib
+pub struct KvStore {}
 
 /// Create new KvStore with HashMap::default()
 impl Default for KvStore {
     fn default() -> Self {
-        KvStore {
-            data: HashMap::default(),
-        }
+        KvStore {}
     }
 }
 
 impl KvStore {
+    /// Open a db file
+    pub fn open(_path: impl Into<PathBuf>) -> Result<KvStore> {
+        unimplemented!();
+    }
+
     /// Create a new KvStore
     pub fn new() -> Self {
-        KvStore {
-            data: HashMap::new(),
-        }
+        KvStore {}
     }
 
     /// Insert or update a K-V pair
-    pub fn set(&mut self, key: String, value: String) {
-        self.data.insert(key, value);
+    pub fn set(&mut self, _key: String, _value: String) -> Result<()> {
+        unimplemented!();
     }
 
     /// Get value by a key
-    pub fn get(&mut self, key: String) -> Option<String> {
-        self.data.get(&key).cloned()
+    pub fn get(&mut self, _key: String) -> Result<Option<String>> {
+        unimplemented!();
     }
 
     /// Remove a K-V pair from KvStore
-    pub fn remove(&mut self, key: String) {
-        self.data.remove(&key);
+    pub fn remove(&mut self, _key: String) -> Result<()> {
+        unimplemented!();
     }
 }
