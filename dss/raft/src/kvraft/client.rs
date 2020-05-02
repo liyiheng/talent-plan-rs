8f53d6a6e22870a14355711744634483a6980b3a
liyiheng <liyihenggnehiyil@gmail.com>
2020-05-02 19:40:47 +0800

    lab3a

--- dss/raft/src/kvraft/client.rs
+++ dss/raft/src/kvraft/client.rs
@@ -1,6 +1,10 @@
-use std::fmt;
-
 use crate::proto::kvraftpb::*;
+use futures::Future;
+use std::fmt;
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::time::Duration;
 
 enum Op {
     Put(String, String),
@@ -11,6 +15,8 @@ pub struct Clerk {
     pub name: String,
     pub servers: Vec<KvClient>,
     // You will have to modify this struct.
+    leader_id: AtomicUsize,
+    req_id: AtomicU64,
 }
 
 impl fmt::Debug for Clerk {
@@ -23,7 +29,17 @@ impl Clerk {
     pub fn new(name: String, servers: Vec<KvClient>) -> Clerk {
         // You'll have to add code here.
         // Clerk { name, servers }
-        crate::your_code_here((name, servers))
+        Clerk {
+            name,
+            servers,
+            leader_id: AtomicUsize::new(0),
+            req_id: AtomicU64::new(0),
+        }
+    }
+
+    fn incr_req_id(&self) -> u64 {
+        let prev = self.req_id.fetch_add(1, Ordering::SeqCst);
+        prev + 1
     }
 
     /// fetch the current value for a key.
@@ -33,17 +49,72 @@ impl Clerk {
     // you can send an RPC with code like this:
     // if let Some(reply) = self.servers[i].get(args).wait() { /* do something */ }
     pub fn get(&self, key: String) -> String {
-        // You will have to modify this function.
-        crate::your_code_here(key)
+        let req_id = self.incr_req_id();
+        let req = GetRequest {
+            key,
+            client: self.name.clone(),
+            req_id,
+        };
+        let server_cnt = self.servers.len();
+        loop {
+            // TODO loop forever?
+            let leader = self.leader_id.load(Ordering::Acquire);
+            info!("GetRequest to leader:{}", leader);
+            let fut = self.servers[leader].get(&req);
+            match fut.wait() {
+                Ok(v) => {
+                    if v.wrong_leader {
+                        let leader = (leader + 1) % server_cnt;
+                        self.leader_id.store(leader, Ordering::Release);
+                        // TODO
+                        std::thread::sleep(Duration::from_millis(50));
+                        continue;
+                    } else if v.err.is_empty() {
+                        return v.value;
+                    } else {
+                        return v.err;
+                    }
+                }
+                Err(e) => return e.to_string(),
+            }
+        }
     }
 
     /// shared by Put and Append.
-    //
-    // you can send an RPC with code like this:
-    // let reply = self.servers[i].put_append(args).unwrap();
     fn put_append(&self, op: Op) {
-        // You will have to modify this function.
-        crate::your_code_here(op)
+        let n_servers = self.servers.len();
+        let req_id = self.incr_req_id();
+        let req = match op {
+            Op::Put(k, v) => PutAppendRequest {
+                key: k,
+                value: v,
+                op: 1,
+                client: self.name.clone(),
+                req_id,
+            },
+            Op::Append(k, v) => PutAppendRequest {
+                key: k,
+                value: v,
+                op: 2,
+                client: self.name.clone(),
+                req_id,
+            },
+        };
+        loop {
+            // FIXME
+            let leader = self.leader_id.load(Ordering::Acquire);
+            let fut = self.servers[leader].put_append(&req);
+            if let Ok(v) = fut.wait() {
+                if v.wrong_leader {
+                    let leader = (leader + 1) % n_servers;
+                    self.leader_id.store(leader, Ordering::Release);
+                    std::thread::sleep(Duration::from_millis(50));
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     pub fn put(&self, key: String, value: String) {
--- dss/raft/src/kvraft/server.rs
+++ dss/raft/src/kvraft/server.rs
@@ -1,7 +1,16 @@
 use crate::proto::kvraftpb::*;
 use crate::raft;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::RwLock;
+use std::time::Duration;
 
+use crate::raft::errors::Error;
+use futures::prelude::*;
 use futures::sync::mpsc::unbounded;
+use futures::sync::oneshot;
+use futures::Stream;
 use labrpc::RpcFuture;
 
 pub struct KvServer {
@@ -10,6 +19,38 @@ pub struct KvServer {
     // snapshot if log grows this big
     maxraftstate: Option<usize>,
     // Your definitions here.
+    data: Arc<RwLock<HashMap<String, String>>>,
+    indexes: Arc<Mutex<HashMap<u64, Command>>>,
+    last_reqs: Arc<Mutex<HashMap<String, u64>>>,
+}
+
+#[derive(Clone, PartialEq, Message)]
+struct Command {
+    #[prost(string, tag = "1")]
+    key: String,
+    #[prost(string, optional, tag = "2")]
+    value: Option<String>,
+    #[prost(bool, tag = "3")]
+    delete: bool,
+    #[prost(int32, tag = "4")]
+    op: i32,
+    #[prost(string, tag = "5")]
+    client: String,
+    #[prost(uint64, tag = "6")]
+    req_id: u64,
+}
+
+impl From<PutAppendRequest> for Command {
+    fn from(req: PutAppendRequest) -> Command {
+        Command {
+            key: req.key,
+            value: Some(req.value),
+            delete: false,
+            req_id: req.req_id,
+            op: req.op,
+            client: req.client,
+        }
+    }
 }
 
 impl KvServer {
@@ -19,12 +60,44 @@ impl KvServer {
         persister: Box<dyn raft::persister::Persister>,
         maxraftstate: Option<usize>,
     ) -> KvServer {
-        // You may need initialization code here.
-
         let (tx, apply_ch) = unbounded();
         let rf = raft::Raft::new(servers, me, persister, tx);
+        let rf = raft::Node::new(rf);
+        let server = KvServer {
+            rf,
+            me,
+            maxraftstate,
+            data: Arc::default(),
+            indexes: Arc::default(),
+            last_reqs: Arc::default(),
+        };
+        let data = server.data.clone();
+        let indexes = server.indexes.clone();
+        let last_reqs = server.last_reqs.clone();
+        std::thread::spawn(move || {
+            let _ = apply_ch.for_each(|msg| {
+                if !msg.command_valid {
+                    return Ok(());
+                }
+                let cmd: Command = labcodec::decode(&msg.command).unwrap();
+                let cmd2 = cmd.clone();
+                let mut req_ids_mutex = last_reqs.lock().unwrap();
+                let req_id = req_ids_mutex.get(&cmd.client);
+                if req_id.is_none() || *req_id.unwrap() < cmd.req_id {
+                    req_ids_mutex.insert(cmd.client.clone(), cmd.req_id);
+                    if cmd.delete {
+                        data.write().unwrap().remove(&cmd.key);
+                    } else {
+                        data.write().unwrap().insert(cmd.key, cmd.value.unwrap());
+                    }
+                }
+                indexes.lock().unwrap().insert(msg.command_index, cmd2);
+                Ok(())
+            });
+            info!("apply_ch receiver finished");
+        });
 
-        crate::your_code_here((rf, maxraftstate, apply_ch))
+        server
     }
 }
 
@@ -37,29 +110,16 @@ impl KvServer {
     }
 }
 
-// Choose concurrency paradigm.
-//
-// You can either drive the kv server by the rpc framework,
-//
-// ```rust
-// struct Node { server: Arc<Mutex<KvServer>> }
-// ```
-//
-// or spawn a new thread runs the kv server and communicate via
-// a channel.
-//
-// ```rust
-// struct Node { sender: Sender<Msg> }
-// ```
+// Use a lock here, an easy implementation
 #[derive(Clone)]
 pub struct Node {
-    // Your definitions here.
+    server: Arc<Mutex<KvServer>>,
 }
 
 impl Node {
     pub fn new(kv: KvServer) -> Node {
-        // Your code here.
-        crate::your_code_here(kv);
+        let server = Arc::new(Mutex::new(kv));
+        Node { server }
     }
 
     /// the tester calls Kill() when a KVServer instance won't
@@ -67,7 +127,7 @@ impl Node {
     /// in Kill(), but it might be convenient to (for example)
     /// turn off debug output from this instance.
     pub fn kill(&self) {
-        // Your code here, if desired.
+        self.server.lock().unwrap().rf.kill();
     }
 
     /// The current term of this peer.
@@ -81,23 +141,141 @@ impl Node {
     }
 
     pub fn get_state(&self) -> raft::State {
-        // Your code here.
-        raft::State {
-            ..Default::default()
-        }
+        self.server.lock().unwrap().rf.get_state()
     }
 }
 
 impl KvService for Node {
     // CAVEATS: Please avoid locking or sleeping here, it may jam the network.
     fn get(&self, arg: GetRequest) -> RpcFuture<GetReply> {
-        // Your code here.
-        crate::your_code_here(arg)
+        let GetRequest {
+            key,
+            client,
+            req_id,
+        } = arg;
+        let cmd = Command {
+            op: 3,
+            key,
+            value: None,
+            delete: false,
+            client,
+            req_id,
+        };
+        let (sender, rx) = oneshot::channel();
+        let server = self.server.clone();
+        std::thread::spawn(move || {
+            let wrong_leader = GetReply {
+                err: String::new(),
+                value: String::new(),
+                wrong_leader: true,
+            };
+            if !server.lock().unwrap().rf.is_leader() {
+                let _ = sender.send(Ok(wrong_leader));
+                return;
+            }
+            let result = server.lock().unwrap().rf.start(&cmd);
+            if let Err(e) = result {
+                if let Error::NotLeader = e {
+                    let _ = sender.send(Ok(wrong_leader));
+                } else {
+                    let _ = sender.send(Err(e));
+                }
+                return;
+            }
+
+            let (index, _term) = result.unwrap();
+            for _ in 0..100 {
+                std::thread::sleep(Duration::from_millis(100));
+                let server = server.lock().unwrap();
+                let mut indexes = server.indexes.lock().unwrap();
+                if !indexes.contains_key(&index) {
+                    continue;
+                }
+                let cmd_applied = indexes.get_mut(&index).take().unwrap();
+                if cmd == *cmd_applied {
+                    let reply_ok = GetReply {
+                        wrong_leader: false,
+                        value: server.data.read().unwrap().get(&cmd.key).unwrap().clone(),
+                        err: "".to_owned(),
+                    };
+                    let _ = sender.send(Ok(reply_ok));
+                    return;
+                } else {
+                    let _ = sender.send(Ok(wrong_leader));
+                    return;
+                }
+            }
+
+            let reply = GetReply {
+                err: "Timeout".to_owned(),
+                wrong_leader: false,
+                value: "".to_owned(),
+            };
+            let _ = sender.send(Ok(reply));
+        });
+        Box::new(rx.then(|reply| match reply {
+            Ok(Ok(reply)) => Ok(reply),
+            Ok(Err(e)) => Err(labrpc::Error::Other(e.to_string())),
+            Err(e) => Err(labrpc::Error::Other(e.to_string())),
+        }))
     }
 
     // CAVEATS: Please avoid locking or sleeping here, it may jam the network.
     fn put_append(&self, arg: PutAppendRequest) -> RpcFuture<PutAppendReply> {
-        // Your code here.
-        crate::your_code_here(arg)
+        let wrong_leader = PutAppendReply {
+            err: "Wrong leader".to_owned(),
+            wrong_leader: true,
+        };
+        let reply_ok = PutAppendReply {
+            err: String::default(),
+            wrong_leader: false,
+        };
+
+        let server = self.server.clone();
+        let cmd = Command::from(arg);
+        let (sender, rx) = oneshot::channel();
+        // TODO thread pool
+        std::thread::spawn(move || {
+            if !server.lock().unwrap().rf.is_leader() {
+                let _ = sender.send(Ok(wrong_leader));
+                return;
+            }
+            let result = server.lock().unwrap().rf.start(&cmd);
+            if let Err(e) = result {
+                if let Error::NotLeader = e {
+                    let _ = sender.send(Ok(wrong_leader));
+                } else {
+                    let _ = sender.send(Err(e));
+                }
+                return;
+            }
+            let (index, _term) = result.unwrap();
+            for _ in 0..100 {
+                std::thread::sleep(Duration::from_millis(100));
+                let server = server.lock().unwrap();
+                let mut indexes = server.indexes.lock().unwrap();
+                if !indexes.contains_key(&index) {
+                    continue;
+                }
+                let cmd_applied = indexes.get_mut(&index).take().unwrap();
+                if cmd == *cmd_applied {
+                    let _ = sender.send(Ok(reply_ok));
+                    return;
+                } else {
+                    let _ = sender.send(Ok(wrong_leader));
+                    return;
+                }
+            }
+
+            let reply = PutAppendReply {
+                err: "Timeout".to_owned(),
+                wrong_leader: false,
+            };
+            let _ = sender.send(Ok(reply));
+        });
+        Box::new(rx.then(|reply| match reply {
+            Ok(Ok(reply)) => Ok(reply),
+            _ => Err(labrpc::Error::Timeout),
+        }))
     }
 }
