24fc068130554b8ecdba90e78ef9acb62614013a
liyiheng <liyihenggnehiyil@gmail.com>
2020-05-23 16:32:51 +0800

    BB4: Thread pool

--- building-block-04/src/main.rs
+++ building-block-04/src/main.rs
@@ -2,6 +2,7 @@ use std::panic::catch_unwind;
 use std::panic::AssertUnwindSafe;
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
+use std::sync::mpsc::Receiver;
 use std::sync::mpsc::Sender;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -20,33 +21,34 @@ enum Msg {
 }
 
 impl ThreadPool {
+    // Create a worker thread and return its JoinHandle
+    fn create_thread(rx: Arc<Mutex<Receiver<Msg>>>) -> JoinHandle<()> {
+        std::thread::spawn(move || loop {
+            let job = rx.lock().unwrap().recv();
+            if job.is_err() {
+                break;
+            }
+            let job = job.unwrap();
+            match job {
+                Msg::Shutdown => break,
+                Msg::Job(f) => {
+                    // Use catch_unwind here to keep all threads in the pool alive
+                    let wrapper = AssertUnwindSafe(f);
+                    if let Err(e) = catch_unwind(move || {
+                        wrapper();
+                    }) {
+                        println!("Error occurred:{:?}", e);
+                    }
+                }
+            };
+        })
+    }
     fn new(threads: u32) -> Result<Self, ()> {
         let (tx, rx) = std::sync::mpsc::channel::<Msg>();
         let rx = Arc::new(Mutex::new(rx));
-        let mut handles = Vec::with_capacity(threads as usize);
-        for _ in 0..threads {
-            let rx1 = rx.clone();
-            let handle = std::thread::spawn(move || loop {
-                let job = rx1.lock().unwrap().recv();
-                if job.is_err() {
-                    break;
-                }
-                let job = job.unwrap();
-                match job {
-                    Msg::Shutdown => break,
-                    Msg::Job(f) => {
-                        // Use catch_unwind here to keep all threads in the pool alive
-                        let wrapper = AssertUnwindSafe(f);
-                        if let Err(e) = catch_unwind(move || {
-                            wrapper();
-                        }) {
-                            println!("Error occurred:{:?}", e);
-                        }
-                    }
-                };
-            });
-            handles.push(Some(handle));
-        }
+        let handles = (0..threads)
+            .map(|_| Some(Self::create_thread(rx.clone())))
+            .collect();
         Ok(ThreadPool {
             handles,
             sender: tx,
@@ -81,7 +83,9 @@ struct JobStatus {
 }
 
 fn main() {
+    // Create a pool with 4 threads
     let pool = ThreadPool::new(4).unwrap();
+    // Send 10 jobs to the pool that would panic
     for _ in 0..10 {
         pool.spawn(move || {
             thread::sleep(Duration::from_millis(50));
@@ -89,10 +93,11 @@ fn main() {
             panic!("Oops!");
         });
     }
+
     let status = Arc::new(JobStatus {
         jobs_completed: AtomicU32::new(0),
     });
-
+    // Run 100 common jobs
     let job_cnt = 100;
     for _ in 0..job_cnt {
         let status_shared = status.clone();
